b2d11850644c17aba1389593627d9d7ea81fbf99
CruZxPog <andredacruzribeiro13@gmail.com>
2023-03-24 11:46:37 +0100

    update 24/03
    I made some updates to verify.php. The file had PHP, JS, and HTML code, and the issue was that the PHP code ran first, followed by the HTML and JS code. As a result, the database update didn't wait for the required 7 seconds and closed too quickly. I fixed this problem by removing the JS and HTML code and using only PHP code, which resolved the issue.
    
    Next, I needed to find a way to let the user know whether or not they could open the door. I added an event listener to the submit button that removes all elements from the page, changes the background color to green or red, and refreshes the page.
    
    Finally, I removed the testcookies.php file, as it was no longer needed.

--- login/testCookies.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $deurnaam_cookie = $_COOKIE['deurnaam'];
-    $deurrank_cookie = $_COOKIE['deurrank'];
-
-    echo $deurnaam_cookie."<br>";
-    echo $deurrank_cookie."<br>";
-?>
--- login/verify.php
+++ login/verify.php
@@ -1,85 +1,37 @@
 <?php
-$link = mysqli_connect("localhost","root","","locks");
+$link = mysqli_connect("localhost", "root", "", "locks");
+
+ob_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // print_r($_POST);
-
     $username = $_POST['username'];
     $username = strtolower($username);
     $password = $_POST['password'];
     $deurnaam_cookie = $_COOKIE['deurnaam'];
     $deurnaam_cookie = strtolower($deurnaam_cookie);
     $deurrank_cookie = $_COOKIE['deurrank'];
-    $sanitized_username = 
-        mysqli_real_escape_string($link, $username);
-        //mysqli_real_escape_string functie neemt de speciale tekens als text en beschouwt ze niet als querygebruik.
-    $sanitized_password = 
-        mysqli_real_escape_string($link, $password);
+    $sanitized_username = mysqli_real_escape_string($link, $username);
+    $sanitized_password = mysqli_real_escape_string($link, $password);
    
-    $sql = "SELECT `rank` FROM `users` WHERE `username`= '$sanitized_username' and `password`= '$sanitized_password'";
-        
-    $result = mysqli_query($link, $sql) 
-        or die(mysqli_error($link));
-        
+    $sql = "SELECT `rank` FROM `users` WHERE `username`='$sanitized_username' AND `password`='$sanitized_password'";
+    $result = mysqli_query($link, $sql) or die(mysqli_error($link));
     $user_rank = mysqli_fetch_array($result);
     
     if($user_rank['rank'] >= $deurrank_cookie) {
-        ?>
-        <body id="background" style="background-color: rgb(19, 136, 8) ">
-        <?php
-           $sql = "UPDATE 'doors' SET 'status'='open' WHERE  'deur naam'='$deurnaam_cookie'";
-           $result = mysqli_query($link, $sql) 
-           or die(mysqli_error($link));
-           $affected_rows = mysqli_affected_rows($link);
-        ?>
-        </body>
-        <script>
-            window.addEventListener("load", () => {
-                const background = document.getElementById("background")
-                let time = 15
-                const interval = setInterval(() => {
-                    time--
-                    console.log(time)
-                    if (time == 7) {
-                        <?php
-                          $sql = "UPDATE 'doors' SET 'status'='close' WHERE 'deur naam'='$deurnaam_cookie'";
-                          $result = mysqli_query($link, $sql) 
-                          or die(mysqli_error($link));
-                          $affected_rows = mysqli_affected_rows($link);
-                        ?>
-                        background.style.backgroundColor = "rgb( 219, 0, 7)"
-                    } 
-                    if (time == 0) {
-                        window.location.replace("scramblepad.html")
-                        clearInterval(interval)
-                    }
-                }, 1000)
-            })
-        </script>
-        <?php
+        $sql = "UPDATE `doors` SET `status`='open' WHERE `deur naam`='$deurnaam_cookie'";
+        $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+        sleep(5); // wait for 5 seconds before updating the status to close
+        $sql = "UPDATE `doors` SET `status`='close' WHERE `deur naam`='$deurnaam_cookie'";
+        $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+        sleep(3);
+        header("Location: scramblepad.html");
+        exit();
     } else {
-        ?>
-        <body id="background" style="background-color:rgb( 219, 0, 7)">
-        </body>
-        <script>
-            window.addEventListener("load", () => {
-                let time = 6
-                const interval = setInterval(() => {
-                    time--
-                    if(time == 0) {
-                        window.location.replace("scramblepad.html")
-                        clearInterval(interval)
-                    }
-                },1000)
-            })
-        </script>
-        <?php
+        // Redirect the user to the scramblepad.html page
+        header("Location: scramblepad.html");
+        exit();
     }
 
-    //als dit niet werkt kijk naar https://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php
-    //use bcrypt to hash password check chat gpt
-
-
+    mysqli_close($link);
 }
-
 ?>
