e957c9d917c9694a9df695b7fa8bbde13e634330
CruZxPog <andredacruzribeiro13@gmail.com>
2023-04-17 22:26:51 +0200

    update 4/17
    added a way for the users to generate a qr-code. this way he can place it at the door and when someone wants to use it they can scan it.

--- setup/setup.php
+++ setup/setup.php
@@ -8,55 +8,88 @@
     <link rel="stylesheet" href="setup_style.css">
   </head>
   <body>
-    <div class="container">
-      <form action="../login/scramblepad.html" onsubmit="setCookies()">
-        <?php
-          // Connect to the database
-          $link = mysqli_connect("localhost", "root", "", "locks");
+    
+    <form action="../login/scramblepad.html" onsubmit="setCookies()">
+      <?php
+        // Connect to the database
+        $link = mysqli_connect("localhost", "root", "", "locks");
 
-          if ($link->connect_error) {
-              die("Connection failed: " . $link->connect_error);
-          }
+        if ($link->connect_error) {
+            die("Connection failed: " . $link->connect_error);
+        }
 
-          // Query the database to get the options for the dropdown menu
-          $sql = "SELECT * FROM `doors`";
-          $result = $link->query($sql);
-          $options = array();
+        // Query the database to get the options for the dropdown menu
+        $sql = "SELECT * FROM `doors`";
+        $result = $link->query($sql);
+        $options = array();
 
-          if ($result->num_rows > 0) {
-              while ($row = $result->fetch_assoc()) {
-                  $option_value = $row["deur naam"];
-                  $option_class = ($row["chipid"] !== "") ? "in-use" : "";
-                  $options[] = array("value" => $option_value, "class" => $option_class);
-              }
-          }
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $option_value = $row["deur naam"];
+                $option_class = ($row["chipid"] !== "") ? "in-use" : "";
+                $options[] = array("value" => $option_value, "class" => $option_class);
+            }
+        }
 
-          // Generate the HTML for the dropdown menu
-          echo '<select id="deurnaam" name="deurrank" required>';
-          echo '<option disabled selected value="">Select your door</option>'; // Add placeholder option
-          foreach ($options as $option) {
-              echo '<option value="' . $option["value"] . '" class="' . $option["class"] . '" ' . ($option["class"] ? 'disabled' : '') . '>' . $option["value"] . '</option>';
-          }
-          echo '</select>';
+        // Generate the HTML for the dropdown menu
+        echo '<select id="deurnaam" name="deurrank" required>';
+        echo '<option disabled selected value="">Select your door</option>'; // Add placeholder option
+        foreach ($options as $option) {
+            echo '<option value="' . $option["value"] . '" class="' . $option["class"] . '" ' . ($option["class"] ? 'disabled' : '') . '>' . $option["value"] . '</option>';
+        }
+        echo '</select>';
 
-          // Close the database connection
-          $link->close();
-        ?>
+        // Close the database connection
+        $link->close();
+      ?>
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <button type="submit" id="submit">Submit</button>
 
+      <button type="button" id="generateQRCode" onclick="generateQRCode()">Generate QR Code</button>
+      <div id="container"></div>
+      <button type="button" id="print-btn">Print QR Code</button>
+    
+    </form>
+   
     <script>
       function setCookies() {
         // Get the values of the input and select fields
         let deurnaamValue = document.getElementById("deurnaam").value.toLowerCase();
-        let deurrankValue = document.getElementById("deurrank").value.toLowerCase();
+        let deurrankValue = document.getElementById("deurrank");
 
         // Set the cookies with the values
         document.cookie = "deurnaam=" + encodeURIComponent(deurnaamValue);
         document.cookie = "deurrank=" + encodeURIComponent(deurrankValue);
       }
+
+      function generateQRCode() {
+        // Get the values of the input and select fields
+        let deurnaamValue = document.getElementById("deurnaam").value.toLowerCase();
+        let deurrankValue = document.getElementById("deurrank");
+
+        // Get the IP address of the host
+        let ipAddress = location.hostname;  
+
+        // Generate the URL with the cookie values
+        let qrURL = `http://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=http%3A%2F%2F${ipAddress}%2Fset_qrCookies.html%3Fdeurnaam%3D${deurnaamValue}%26deurrank%3D${deurrankValue}`;
+
+        // Create a new image element with the QR code as the source
+        let qrImage = document.createElement("img");
+        qrImage.src = qrURL;
+
+        // Append the QR code to the container
+        let container = document.getElementById("container");
+        container.appendChild(qrImage);
+
+        printBtn.style.display = "block";
+      }
+
+      const printBtn = document.getElementById("print-btn");
+printBtn.addEventListener("click", () => {
+  window.print();
+});
+
+
     </script>
   </body>
 </html>
