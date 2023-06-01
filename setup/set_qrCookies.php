51b4036c9e568c8aebabc4ec65428423845c68ff
CruZxPog <andredacruzribeiro13@gmail.com>
2023-06-01 23:44:31 +0200

    laatste commit
    het project is klaar dit is de laatste commit (hopelijk)

--- setup/set_qrCookies.php
+++ setup/set_qrCookies.php
@@ -3,6 +3,6 @@
 $deurnaam = $_GET['deurnaam'];
 // Set the cookies separately, with a distant future expiration date
 setcookie('deurnaam', $deurnaam, time() + (10 * 365 * 24 * 60 * 60), '/');
-header("Location: ..\login\scramblepad.php");
+header("Location: ..\login\scramblepad.html");
 ?>
 
